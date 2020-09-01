5a9adfd31cbd9caff535feafbb89d5f375628990
Pablyco <Pablycohacker@gmail.com>
2020-09-01 19:24:45 -0300

    joystick movement android builds settings

---
+++ Mental Havoc/Assets/Scripts/JoystickMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickMovement : MonoBehaviour
+{
+       public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Joystick joystick;
+
+   private void Update() 
+{
+    if (joystick.Horizontal >= .2f)
+    {
+
+   movement.x = moveSpeed;
+        
+    }
+    else if (joystick.Horizontal <= -.2f)
+    
+    {
+        movement.x = -moveSpeed;
+    }
+    else
+    {
+        movement.x = 0f;
+    }
+
+
+
+    
+}
+
+void FixedUpdate()
+{
+    
+    rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
+}
+}
