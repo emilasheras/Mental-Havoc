fa2a0c1a524668397a427f610a3fb0b3694c93ad
Pablyco <Pablycohacker@gmail.com>
2020-09-02 20:00:00 -0300

    Touch Inputs

---
+++ Mental Havoc/Assets/Scripts/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform FirePoint;
+    public GameObject BulletPrefab;
+
+
+    private void Update()
+    {
+
+
+     if (Input.touchCount > 0)
+
+     {
+        
+         Debug.Log(Input.touchCount);
+
+     }
+
+
+
+    }
+    void Shoot()
+    {
+        
+        Instantiate (BulletPrefab, FirePoint.position, FirePoint.rotation);
+
+    }
+}
