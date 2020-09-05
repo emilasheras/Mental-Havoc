8416c5588312824cd8b6d6a8a91d7659d66c9fba
Pablyco <Pablycohacker@gmail.com>
2020-09-05 12:06:28 -0300

    ABC

---
+++ Mental Havoc/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3f;
+    public Rigidbody2D rb;
+    Vector2 movedown;
+    
+
+    void Update()
+    {
+        movedown.y = -speed;
+
+    }
+    private void FixedUpdate() 
+    {
+     rb.MovePosition(rb.position + movedown * speed * Time.deltaTime);  
+    }
+}
