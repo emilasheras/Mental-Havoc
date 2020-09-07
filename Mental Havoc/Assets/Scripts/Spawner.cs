d74b5a70ffcf070d40cf8f816037bb08808bcdc1
Pablyco <Pablycohacker@gmail.com>
2020-09-07 19:09:18 -0300

    Spawner v1.0

---
+++ Mental Havoc/Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    
+    public Transform SpawnPoint;
+    public GameObject[] Enemies;
+
+    private int rand;
+
+    public float StartTimeBtwSpawns;
+    private float TimeBtwSpawns;
+
+
+    private void Start() 
+    {
+
+        TimeBtwSpawns = StartTimeBtwSpawns;
+
+    }
+    private void Update() 
+    {
+
+        if (TimeBtwSpawns <= 0)
+
+        {  
+          Instantiate(Enemies[0],SpawnPoint.transform.position, Quaternion.identity);
+        }
+
+
+    }
+}
