a710f8f4b7f039e3fecc85622ec0e1d17da66a44
Pablyco <Pablycohacker@gmail.com>
2020-09-14 19:39:34 -0300

    ya se pueden crear items

---
+++ Mental Havoc/Assets/Scripts/Scriptable Objects/CreateNewItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class CreateNewItem : ScriptableObject
+{
+   
+   //crea un item con estos valores
+    public float Damage;
+    public float Critical;
+    public int Health;
+    public float DropChance;
+
+
+}
